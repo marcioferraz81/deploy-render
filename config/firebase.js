ea62ebc2b5e44bd123d16e7aa637f414386b2767
maercioferraz81 <aulaskodular2024@gmail.com>
2026-03-19 15:34:24 -0300

    acertos json

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,9 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json"); // chave do firebase
+//local
+//const serviceAccount = require("./serviceAccountKey.json"); // chave do firebase
 
+//para render
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://web-60e46-default-rtdb.firebaseio.com/",
